6c0feec69d82cec4d5487cf5228da4781ca11530
Nikita Pytalev <nikita.pytalev@yahoo.com>
2018-03-14 20:57:54 +0000

    FSM(not finished)
    So far it can't chage states.

---
+++ EvacuationSimulation/Assets/MinionStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionStats : ScriptableObject {
+
+    [Range(0,100)]
+    int health = 100; 
+}
---
+++ EvacuationSimulation/Assets/Scripts/StateController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour 
+{
+
+    public State currentState;
+
+
+	void Update () 
+    {
+        currentState.UpdateState(this);
+	}
+}
